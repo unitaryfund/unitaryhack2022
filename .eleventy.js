f5db9a98c9790b5e2e5cbdc6ff1211b7c6bd7fcc
Sarah Kaiser <sckaiser@sckaiser.com>
2022-03-24 01:22:02 -0700

    fixing fonts

--- .eleventy.js
+++ .eleventy.js
@@ -78,6 +78,7 @@ module.exports = function(eleventyConfig) {
     // Don't process folders with static assets e.g. images
     eleventyConfig.addPassthroughCopy("favicon.ico");
     eleventyConfig.addPassthroughCopy("static/img");
+    eleventyConfig.addPassthroughCopy("static/fonts");
     eleventyConfig.addPassthroughCopy("admin");
     eleventyConfig.addPassthroughCopy("_includes/assets/");
 
